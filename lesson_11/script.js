0e20c571b0c8c3bf9872c590fb1f4669c1a58557
vahabrat <62399153+vahabrat@users.noreply.github.com>
2020-04-22 13:40:22 +0300

    burger_menu_adaptive

---
+++ lesson_11/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.menu-button').click(function(event){
+        $('.menu-button,.header-content__nav').toggleClass('active');
+    });
+});
+
+
+/*
+document.getElementById('hamburger').click(() => {
+	document.getElementById('hamburger_menu').classList.toggle('active');
+});
+*/
+
+
+
